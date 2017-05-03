44fc23f0c7d82e2765e898dd135d5721242f7fc8
Ken <kenaramaki@gmail.com>
2017-05-03 20:44:07 -0300

    RetroCalculator - added a new font and a sound to the buttons

--- Foundational-iOS/RetroCalculator/RetroCalculator/ViewController.swift
+++ Foundational-iOS/RetroCalculator/RetroCalculator/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    var btnSound : AVAudioPlayer!
+    
+    
+    // MARK: - Outlet
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        
+        }catch let err as NSError {
+            
+            print(err.debugDescription)
+            
+        }
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    // MARK: - Methods
+    func playSound() {
+        
+        if btnSound.isPlaying{
+            btnSound.stop()
+        }
+    
+        btnSound.play()
+        
     }
-
-
+    
+    
+    
+    
+    
+    // MARK: - Actions
+    @IBAction func numberPressed(sender: UIButton) {
+        
+        playSound()
+        
+    }
+    
+    
 }
 
