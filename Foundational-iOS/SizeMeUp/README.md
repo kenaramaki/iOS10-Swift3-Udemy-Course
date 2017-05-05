8b951a556d03244ab575c20739905f7ce40a54c4
Ken <kenaramaki@gmail.com>
2017-05-04 22:56:37 -0300

    Size classes

---
+++ Foundational-iOS/SizeMeUp/README.md
@@ -0,0 +1,3 @@
+Working with Size Classes
+
+
---
+++ Foundational-iOS/SizeMeUp/SizeMeUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeMeUp
+//
+//  Created by Árthur Ken Aramaki Mota on 04/05/17.
+//  Copyright © 2017 Árthur Ken Aramaki Mota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
