12c992a9030e395e4d88b5c60d5179b7aaa849f2
Ken <kenaramaki@gmail.com>
2017-05-11 21:58:54 -0300

    Party Rock App - Showing YoutTube videos in a web view

--- Foundational-iOS/PartyRockApp/PartyRockApp/VideoVC.swift
+++ Foundational-iOS/PartyRockApp/PartyRockApp/VideoVC.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class VideoVC: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    // MARK: - Properties
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        titleLbl.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
     
     }
 
